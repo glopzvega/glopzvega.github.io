d66beba0d940d205d59c2480f96e1108d93e4fb0
mcgalv <mcgalv@gmail.com>
2014-12-18 00:50:21 -0600

    Design and Responsive

--- js/profile.js
+++ js/profile.js
@@ -7,5 +7,24 @@ $(function(){
 		newActive.addClass("active");
 	});
 
+	$(".experience .exp").hover(function()
+		{
+			$(this).addClass("exp_hover");			
+			$(this).find(".description h3 span").addClass("exp_span_hover")			
+			$(this).find("img").addClass("exp_img_hover");
+
+		}, function(){
+			$(this).removeClass("exp_hover");
+			$(this).find(".description h3 span").removeClass("exp_span_hover")			
+			$(this).find("img").removeClass("exp_img_hover");
+		});
+
+	// $(".exp").on("hover", function(){
+
+	// 	$(this).addClass("exp_hover");
+	// 	$(this).find("img").addClass("exp_img_hover");
+
+	// });
+
 
 });
